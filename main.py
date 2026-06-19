10b4a7a757b17a462019ad0bd57b07a2ba2d3e2d
mudasir1134 <razamudasir1134@gmail.com>
2026-06-19 15:00:34 +0500

    Implement expression tokenizer

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from tokenizer import tokenize
+
+
 def main():
     expression = input("Enter Expression: ")
 
-    print("\nExpression Entered:")
-    print(expression)
+    tokens = tokenize(expression)
+
+    print("\nTokens:")
+    print(tokens)
+
 
 if __name__ == "__main__":
     main()
