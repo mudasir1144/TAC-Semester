a0645cd4c9d4c9220a4aa66561cfa477af8e1710
mudasir1134 <razamudasir1134@gmail.com>
2026-06-20 12:12:55 +0500

    Comments addec

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from tokenizer import tokenize
+#Imported tokenizer to divide expression in token
+from tokenizer import tokenize 
 from tac_generator import generate_tac
 
-
+#Defining a function
 def main():
 
     expression = input("Enter Expression: ")
+    #Send expression to tokenize function , convert expression to token
 
     tokens = tokenize(expression)
 
     print("\nTokens:")
     print(tokens)
 
-    generate_multiplication_tac(tokens)
+    print("\nThree Address Code:")
+    #send token to function to generate_tac
+    generate_tac(tokens)
 
-
+#Check whether the file is directly running or imported 
+#if directly run then runs in (main)
 if __name__ == "__main__":
     main()
